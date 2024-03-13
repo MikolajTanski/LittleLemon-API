6153f6cc1c8c68e1d05ac5c608f5ff7ea43a1469
Szczepan <szczep.kowalik@gmail.com>
2024-03-13 19:17:08 +0100

    added models

---
+++ Models/Meal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LittleLemon_API.Models
+{
+    public class Meal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Meal name is required")]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public float Price { get; set; }
+        
+    }
+}
